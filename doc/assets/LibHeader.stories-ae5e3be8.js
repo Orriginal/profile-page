a86f75ff19260158c0f7749f4f47a1a582b707fd
Jan Kuipers <Orriginal@live.nl>
2023-08-08 22:09:55 +0200

    [TASK] Update dist storybook

---
+++ doc/assets/LibHeader.stories-ae5e3be8.js
@@ -0,0 +1,17 @@
+import{_ as d}from"./LibHeader-010df7ca.js";import"./vue.esm-bundler-caa660e7.js";const b={title:"library/LibHeader",component:d,tags:["autodocs"],argTypes:{label:{control:"text"},size:{control:"select",options:["text-xl","text-2xl","text-4xl"]}},args:{}},e={args:{label:"Header Label",size:"text-xl"}},r={args:{label:"Larger Header Label",size:"text-2xl"}},a={args:{label:"Larger Header Label",size:"text-4xl"}};var s,t,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    label: 'Header Label',
+    size: 'text-xl'
+  }
+}`,...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var l,n,c;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: 'Larger Header Label',
+    size: 'text-2xl'
+  }
+}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var i,g,x;a.parameters={...a.parameters,docs:{...(i=a.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  args: {
+    label: 'Larger Header Label',
+    size: 'text-4xl'
+  }
+}`,...(x=(g=a.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};const z=["SizeXl","LargerSize","BiggestSize"];export{a as BiggestSize,r as LargerSize,e as SizeXl,z as __namedExportsOrder,b as default};
+//# sourceMappingURL=LibHeader.stories-ae5e3be8.js.map
